b097f27f311bb5e101818c249ef256c46528ba8c
ASK Computers <pfulsoundar@gmail.com>
2020-01-23 22:16:29 +0530

    Code changes

---
+++ tech-test-master_Coding_Test/TechTest/AnyCompany/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AnyCompany
+{
+    public static class CustomerRepository
+    {
+        private static string ConnectionString = @"Data Source=DESKTOP-21TVJEC;Initial Catalog=AnyCompany;Integrated Security=True;Persist Security Info=False;Enlist=False;Pooling=True;Min Pool Size=1;Max Pool Size=100;Connect Timeout=15;User Instance=False";//ConfigurationSettings.AppSettings["AnyCompany"].ToString();//@"Data Source=(local);Database=Customers;User Id=admin;Password=password;";
+
+        public static Customer Load(int customerId)
+        {
+            Customer customer = new Customer();
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Customer WHERE CustomerId = " + customerId,
+                connection);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                customer.Name = reader["Name"].ToString();
+                customer.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                customer.Country = reader["Country"].ToString();
+            }
+
+            connection.Close();
+
+            return customer;
+        }
+      
+    }
+}
---
+++ tech-test-master_Coding_Test/TechTest/AnyCompany/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace AnyCompany
+{
+    public class OrderRepository
+    {
+        private static string ConnectionString = @"Data Source=DESKTOP-21TVJEC;Initial Catalog=AnyCompany;Integrated Security=True;Persist Security Info=False;Enlist=False;Pooling=True;Min Pool Size=1;Max Pool Size=100;Connect Timeout=15;User Instance=False";//ConfigurationSettings.AppSettings["AnyCompany"].ToString();// @"Data Source=(local);Database=Orders;User Id=admin;Password=password;";
+
+        public void Save(Order order)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO Orders VALUES (@OrderId, @Amount, @VAT,@CustomerId)", connection);
+
+            command.Parameters.AddWithValue("@OrderId", order.OrderId);
+            command.Parameters.AddWithValue("@Amount", order.Amount);
+            command.Parameters.AddWithValue("@VAT", order.VAT);
+            command.Parameters.AddWithValue("@CustomerId", order.CustomerId);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
